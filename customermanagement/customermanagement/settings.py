0de455be53b2a600e1d8f76611ba25b18d3f9f5e
tanaya123 <tanayawankar58@gmail.com>
2020-10-20 04:40:57 +0530

    sixth commit

--- customermanagement/customermanagement/settings.py
+++ customermanagement/customermanagement/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'youremailid'
+EMAIL_HOST_USER = 'youremail'
 EMAIL_HOST_PASSWORD = 'yourpassword'
