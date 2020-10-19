73229845ab0abddc72e3e40779d2b7e278f0014d
tanaya123 <tanayawankar58@gmail.com>
2020-10-20 04:32:08 +0530

    fifth commit

--- customermanagement/customermanagement/settings.py
+++ customermanagement/customermanagement/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '$t!x49rj2h*=i#juausca^q6$%z^rri!(l_m23v!4i7&$==i4='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
